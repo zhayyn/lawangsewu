87698679024dee414f15980d60226ba4d663ba8c
zhayyn <zhayyudin@gmail.com>
2026-03-01 09:34:14 +0700

    ┌   aicommits [?25l│ ○  Detecting staged files [999D[2A[J[J│ ◇  Detected 2 staged files:      .github/autocommit.sh      logs/generator.log [?25h│ └  ✖ Please set your OpenAI API key via `aicommits config set OPENAI_KEY=<your token>`

---
+++ .github/autocommit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Add all new and modified files to the staging area
+git add .
+
+# Generate a commit message using aicommits
+# The -g flag generates the message and aicommits exits
+COMMIT_MESSAGE=$(aicommits -g)
+
+# Commit the changes
+git commit -m "$COMMIT_MESSAGE"
